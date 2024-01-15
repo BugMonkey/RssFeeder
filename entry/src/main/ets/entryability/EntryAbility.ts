0d19d6bfcddd7ed36902f5a3d8e73fd6b95110e7
BugMonkey <15298353932@163.com>
2024-01-15 20:33:57 +0800

    我的订阅页面完善

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -1,34 +1,30 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import hilog from '@ohos.hilog';
 import window from '@ohos.window';
-// 导入KV数据库模块
-import distributedKVStore from '@ohos.data.distributedKVStore';
-import relationalStore from '@ohos.data.relationalStore';
 
-export var kvManager:distributedKVStore.KVManager;
+
 export default class EntryAbility extends UIAbility {
+
 
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
     let context = this.context;
-    const kvManagerConfig = {
-      context:context,
-      bundleName:'com.cheercola.rssfeeder'
-    };
-    try {
-      kvManager = distributedKVStore.createKVManager(kvManagerConfig)
-      console.error('创建kvmanager成功')
-    }catch (e){
-      console.error('创建kvmanager失败')
-    }
 
-    const STORE_CONFIG = {
-      name: 'Rss.db', // 数据库文件名
-      securityLevel: relationalStore.SecurityLevel.S1 // 数据库安全级别
-    };
-
-
+    //数据库初始
+    // const kvManagerConfig = {
+    //   context:context,
+    //   bundleName:'com.cheercola.rssfeeder'
+    // };
+    // try {
+    //   kvManager = distributedKVStore.createKVManager(kvManagerConfig)
+    //   console.error('创建kvmanager成功')
+    // }catch (e){
+    //   console.error('创建kvmanager失败')
+    // }
+    //  模拟器上不能用
+    // PersistentStorage.PersistProp('aProp', 47);
   }
+
 
   onDestroy() {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
