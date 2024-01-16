2366cddfd75a981663255b0566b14b7f77c344ab
CheerCoke <133180816+CheerCoke@users.noreply.github.com>
2024-01-17 00:18:10 +0800

    页面结构修改

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -4,8 +4,6 @@ import window from '@ohos.window';
 
 
 export default class EntryAbility extends UIAbility {
-
-
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
     let context = this.context;
@@ -25,14 +23,38 @@ export default class EntryAbility extends UIAbility {
     // PersistentStorage.PersistProp('aProp', 47);
   }
 
+  async enterImmersion(windowClass: window.Window) {
+    // 获取状态栏和导航栏的高度
+    windowClass.on("avoidAreaChange", ({ type, area }) => {
+      if (type == window.AvoidAreaType.TYPE_SYSTEM) {
+        // 将状态栏和导航栏的高度保存在AppStorage中
+        AppStorage.SetOrCreate<number>("topHeight", area.topRect.height);
+        AppStorage.SetOrCreate<number>("bottomHeight", area.bottomRect.height);
+      }
+    })
+    // 设置窗口布局为沉浸式布局
+    // await windowClass.setWindowLayoutFullScreen(true)
+    // await windowClass.setWindowSystemBarEnable(["status", "navigation"])
+    // 设置状态栏和导航栏的背景为透明
+    await windowClass.setWindowSystemBarProperties({
+      navigationBarColor: "#00000000",
+      statusBarColor: "#00000000",
+      navigationBarContentColor: "#FF0000",
+      statusBarContentColor: "#FF0000"
+    })
+  }
+
 
   onDestroy() {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage) {
+   onWindowStageCreate(windowStage: window.WindowStage) {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+    windowStage.getMainWindow().then((windowClass)=>{
+       this.enterImmersion(windowClass);
+    })
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
